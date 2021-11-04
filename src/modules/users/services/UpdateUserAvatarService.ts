ed034592851d521de2ae6376cf2e97746db1a44a
Stharley Maxwell <stharleymax@gmail.com>
2021-11-04 16:36:34 -0300

    create service updateUserAvatar

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/appError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import path from 'path';
+import uploadConfig from '@config/uploads';
+import fs from 'fs';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExist = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExist) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
